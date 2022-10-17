98002676406d600dfeead60ba3437ac423c8ce8e
ManishV5 <bhagat.manishV5@gmail.com>
2022-10-17 14:27:44 +0530

    created UI for subscribe and sell pages

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -1,3 +1,46 @@
 export const Sell = () => {
-    return (<h1>Sell</h1>)
+    return (
+        <div className="container d-flex justify-content-center col-8 mt-5">
+            <div className="addNewListing">
+                <div class="text-center">
+                    <label className="form-label">Add new listing</label>
+                </div>
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Listing</span>
+                    <input type="text" className="form-control" placeholder="Enter listing name"/>
+                    <span className="input-group-text">Location</span>
+                    <input type="text" className="form-control" placeholder="Enter location"/>
+                </div>
+
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Price (in Rs)</span>
+                    <input type="number" className="form-control"/>
+                    <span className="input-group-text">Size (in sq.)</span>
+                    <input type="number" className="form-control"/>
+                    <div class="form-check ms-3">
+                        <label class="form-check-label" for="flexCheckDefault">Negotiable</label>
+                        <input class="form-check-input" type="checkbox" value=""/>
+                    </div>
+                    <div class="form-check ms-3">
+                        <label class="form-check-label" for="flexCheckDefault">Financing</label>
+                        <input class="form-check-input" type="checkbox" value=""/>
+                    </div>
+                </div>
+
+                
+                <div className="input-group mb-3">
+                    <span className="input-group-text">Description</span>
+                    <textarea className="form-control" aria-label="With textarea"></textarea>
+                </div>
+
+                <div className="input-group mb-3">
+                    <label class="input-group-text">Upload Images</label>
+                    <input type="file" class="form-control"/>
+                </div>
+                <div class="mt-3 text-center">
+                    <button class="btn btn-outline-primary me-3">Add Listing</button>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/pages/Subscribe.js
+++ src/pages/Subscribe.js
@@ -1,3 +1,63 @@
+import smallImage from "../assets/small.jpg"
+import mediumImage from "../assets/medium.jpg"
+import largeImage from "../assets/large.jpg"
+
 export const Subscribe = () => {
-    return (<h1>Subscribe</h1>)
+    return (
+        <div className="container">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                    <img src={smallImage} class="card-img-top" alt="plan logo"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Basic (Rs 99/Month)</h5>
+                        <p class="card-text">Makes it easier for you to contact property seller</p>
+                        <ul>
+                            <li>Basic limit of contacting 10 sellers a month</li>
+                            <li>Listing property limit of 1 per month</li>
+                            <li>Get seller details to contact</li>
+                        </ul>
+                        <div className="text-center">
+                            <button className="btn btn-danger">Subscribe</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                    <img src={mediumImage} class="card-img-top" alt="plan logo"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Plus (Rs 199/Month)</h5>
+                        <p class="card-text">Upgraded limits to improve your experience even more</p>
+                        <ul>
+                            <li>Increased limit of contacting 25 sellers a month</li>
+                            <li>Listing property limit of 2 per month</li>
+                            <li>includes benefits of previous tier</li>
+                        </ul>
+                        <div className="text-center">
+                            <button className="btn btn-danger">Subscribe</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                    <img src={largeImage} class="card-img-top" alt="plan logo"/>
+                    <div class="card-body">
+                        <h5 class="card-title">Ultimate (Rs 499/Month)</h5>
+                        <p class="card-text">Unlocks your potential.</p>
+                        <ul>
+                            <li>No limits for either contancting seller or posting listings</li>
+                            <li>Dedicated 24 x 7 support line to resolve your issues</li>
+                            <li>includes benefits of previous tier</li>
+                        </ul>
+                        <div className="text-center">
+                            <button className="btn btn-danger">Subscribe</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
