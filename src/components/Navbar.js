b559508352f0ac8d98d28ca6dea9d6fbe41fc740
Dinith Asokan <dinith.asokan@quantiphi.com>
2022-10-20 10:21:38 +0530

    dropdown changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,117 @@
-import logo from "../assets/logo.jpg"
-import {Link, useNavigate, useLocation} from "react-router-dom"
-import { signOut } from "firebase/auth"
-import { auth } from "../firebase-config/config"
+import logo from "../assets/logo.jpg";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase-config/config";
 
 export const Navbar = (props) => {
-  const location = useLocation()
-  let navigate = useNavigate()
+  const location = useLocation();
+  let navigate = useNavigate();
 
   const handleAdminSignIn = () => {
-    navigate('/adminlogin')
-  }
+    navigate("/adminlogin");
+  };
 
   const handleSignIn = () => {
-    navigate('/signin')
-  }
+    navigate("/signin");
+  };
 
   const handleSignUp = () => {
-    navigate('/signup')
-  }
+    navigate("/signup");
+  };
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
-      localStorage.clear()
-      props.setIsAuth(false)
-      navigate("/")
-    })
+      localStorage.clear();
+      props.setIsAuth(false);
+      navigate("/");
+    });
+  };
+
+  const toggleMenu = () =>{
+    let subMenu = document.getElementById("subMenu")
+    subMenu.classList.toggle("open-menu")
   }
 
-    return (
-        <nav className="navbar navbar-expand-lg bg-light">
-        <div className="container-fluid">
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item mt-1">
-                <Link to="/"><img src={logo} style={{height:"40px", width:"60px"}}/></Link>
-                <span className="brand">Property Portal</span>
-              </li>
-              <li className="nav-item ms-3">
-                <Link className="nav-link" to="/buy">Buy</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/sell">Sell</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/subscribe">Subscription</Link>
-              </li>
-            </ul>
-            {props.isAuth ? (
-              <>
-              <div className="me-2"> Hello, {props.username} !</div>
-              <button className="btn btn-outline-danger" onClick={handleSignOut}>Sign Out</button>
+  const handleTransaction = () => {
+    navigate("/transaction")
+  }
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item mt-1">
+              <Link to="/">
+                <img src={logo} style={{ height: "40px", width: "60px" }} />
+              </Link>
+              <span className="brand">Property Portal</span>
+            </li>
+            <li className="nav-item ms-3">
+              <Link className="nav-link" to="/buy">
+                Buy
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sell">
+                Sell
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/subscribe">
+                Subscription
+              </Link>
+            </li>
+          </ul>
+          {props.isAuth ? (
+            <>
+              <div className="me-2" onClick={toggleMenu}> Hello, {props.username} !</div>
+              <div className="sub-menu-wrap" id="subMenu">
+                <div className="sub-menu">
+                  <a href="#" className="sub-menu-link">
+                    <p onClick={handleTransaction}>My transactions</p>
+                  </a>
+                </div>
+              </div>
+              <button
+                className="btn btn-outline-danger"
+                onClick={handleSignOut}
+              >
+                Sign Out
+              </button>
             </>
-            
-            ) : (
-              <>
-              {
-                location.pathname !== "/adminlogin" && 
-                <button className="btn btn-outline-danger" onClick={handleAdminSignIn}>Admin</button>
-              }
+          ) : (
+            <>
+              {location.pathname !== "/adminlogin" && (
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={handleAdminSignIn}
+                >
+                  Admin
+                </button>
+              )}
 
-              {
-                location.pathname !== "/signin" &&
-                <button className="btn btn-outline-success ms-2" onClick={handleSignIn}>Sign In</button>
-              }
+              {location.pathname !== "/signin" && (
+                <button
+                  className="btn btn-outline-success ms-2"
+                  onClick={handleSignIn}
+                >
+                  Sign In
+                </button>
+              )}
 
-              {
-                location.pathname !== "/signup" &&
-                <button className="btn btn-outline-success ms-2" onClick={handleSignUp}>Sign Up</button>
-              }
-              </>
-            )}
-  
-          </div>
+              {location.pathname !== "/signup" && (
+                <button
+                  className="btn btn-outline-success ms-2"
+                  onClick={handleSignUp}
+                >
+                  Sign Up
+                </button>
+              )}
+            </>
+          )}
         </div>
-      </nav>
-    )
-}
+      </div>
+    </nav>
+  );
+};
