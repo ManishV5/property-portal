a76cde2746e35c8dca68647db5bfa1019caabc9e
ManishV5 <bhagat.manishV5@gmail.com>
2022-10-20 14:27:16 +0530

    restructured transactions page

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,9 +1,42 @@
-import { MyTransaction } from "../components/MyTransactions"
 
 export const Transaction = () => {
-    return (<>
-        <h1 style={{textAlign: "center"}}>My Transaction(s)</h1>
-        <MyTransaction />
-        </>
-    )
+    return (
+      <div className="container mt-5">
+        {/* <h1 style={{ textAlign: "center" }}>My Transaction(s)</h1> */}
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Buyer</th>
+                <th scope="col">Seller</th>
+                <th scope="col">Listing name</th>
+                <th scope="col">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>500000</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@twitter</td>
+                <td>@fat</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    );
 }
